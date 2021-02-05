84f82dd22f859728244a4fc49e042b8088d4246a
johansonfelix <48577361+johansonfelix@users.noreply.github.com>
2021-02-05 10:13:38 -0500

    Test

---
+++ src/main/java/impl/Albums.java
@@ -0,0 +1,30 @@
+package impl;
+
+import pojo.Album;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Albums {
+    HashMap<String, Album> albums = new HashMap<>();
+    public void addAlbum(Album album){
+        albums.put(album.getISRC(), album);
+    }
+    public void deleteAlbum(String ISRC){
+        albums.remove(ISRC);
+    }
+    public Album getAlbum(String ISRC){
+        return albums.get(ISRC);
+    }
+    public String toString(){
+        String str = "";
+        Iterator it = albums.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            str += "ISRC: " + pair.getKey() + ", Title: " + ((Album)pair.getValue()).getTitle() + " | ";
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        return str;
+    }
+}
---
+++ src/main/java/impl/Artists.java
@@ -0,0 +1,33 @@
+package impl;
+
+import pojo.Artist;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Artists {
+    HashMap<String, Artist> artists = new HashMap<>();
+    public void addArtist(Artist artist){
+        artists.put(artist.getNickName(), artist);
+    }
+    public Artist getArtist(String nickname){
+        return artists.get(nickname);
+    }
+    public void deleteArtist(String nickname){
+        artists.remove(nickname);
+    }
+    public String toString(){
+        String str = "";
+        Iterator it = artists.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            str += "Nickname: " + pair.getKey() + ", Full name: " + ((Artist)pair.getValue()).getFirstName()+ " " + ((Artist)pair.getValue()).getLastName() + " | ";
+            it.remove(); // avoids a ConcurrentModificationException
+
+        }
+        return str;
+    }
+
+}
